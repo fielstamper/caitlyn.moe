4828ecc9a4512f6a341b2de50cdb218e466f27c3
fielstamper <161728157+fielstamper@users.noreply.github.com>
2025-09-17 13:48:19 +0800

    adasda

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     image: {
-        domains: ["github.com", "codeberg.org"]
+        domains: ["github.com", "codeberg.org", "x.com"]
     }
 });
